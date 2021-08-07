3a964b4b91941015e414e94851f36dd25f3b9ed3
Samruddhi Deshmukh <dsamruddhi21@gmail.com>
2021-08-07 21:30:50 +0800

    Added code to solve inverse problem (get reconstruction)

---
+++ inverse_problem/inverse.py
@@ -0,0 +1,88 @@
+import numpy as np
+from scipy.special import jv as bessel1
+from scipy.special import hankel1
+
+from config import Config
+from utils.doi_utils import DOIUtils
+
+
+class LinearInverse:
+
+    def __init__(self):
+
+        # System parameters
+        self.frequency = Config.system["frequency"]
+        self.wavelength = 3e8 / self.frequency
+        self.wave_number = 2*np.pi / self.wavelength
+        self.impedance = 120*np.pi
+
+        # Room parameters
+        self.m = Config.doi["inverse_grids"]
+        self.number_of_grids = self.m ** 2
+        self.grid_positions = DOIUtils.get_grid_centroids("inverse")
+        self.grid_radius = DOIUtils.get_grid_radius("inverse")
+
+        # Sensor parameters
+        self.transceiver = Config.sensors["transceivers"]
+        self.number_of_rx = Config.sensors["count"]
+        self.number_of_tx = Config.sensors["count"]
+        self.sensor_positions = Config.sensors["positions"]
+
+        self.nan_remove = True
+        self.noise_level = 0
+
+    def get_direct_field(self):
+        """
+        Field from transmitter to receiver
+        Output dimension - number of transmitters x number of receivers
+        """
+        receiver_x = [pos[0] for pos in self.sensor_positions]
+        receiver_y = [pos[1] for pos in self.sensor_positions]
+
+        transmitter_x = [pos[0] for pos in self.sensor_positions]
+        transmitter_y = [pos[1] for pos in self.sensor_positions]
+
+        [xtd, xrd] = np.meshgrid(transmitter_x, receiver_x)
+        [ytd, yrd] = np.meshgrid(transmitter_y, receiver_y)
+        dist = np.sqrt((xtd - xrd) ** 2 + (ytd - yrd) ** 2)
+        direct_field = (1j / 4) * hankel1(0, self.wave_number * dist)
+        return direct_field
+
+    def get_incident_field(self):
+        """
+        Field from transmitter on every incident grid
+        Output dimension - number of transmitters x number of grids
+        """
+        transmitter_x = [pos[0] for pos in self.sensor_positions]
+        transmitter_y = [pos[1] for pos in self.sensor_positions]
+
+        grid_x = self.grid_positions[0]
+        grid_x = grid_x.reshape(grid_x.size, order='F')
+
+        grid_y = self.grid_positions[1]
+        grid_y = grid_y.reshape(grid_y.size, order='F')
+
+        [xti, xsi] = np.meshgrid(transmitter_x, grid_x)
+        [yti, ysi] = np.meshgrid(transmitter_y, grid_y)
+
+        dist = np.sqrt((xti - xsi)**2 + (yti - ysi)**2)
+        incident_field = (1j/4) * hankel1(0, self.wave_number * dist)
+        return incident_field
+
+    def get_greens_integral(self):
+        transmitter_x = [pos[0] for pos in self.sensor_positions]
+        transmitter_y = [pos[1] for pos in self.sensor_positions]
+
+        grid_x = self.grid_positions[0]
+        grid_x = grid_x.reshape(grid_x.size, order='F')
+
+        grid_y = self.grid_positions[1]
+        grid_y = grid_y.reshape(grid_y.size, order='F')
+
+        [xtg, xsg] = np.meshgrid(transmitter_x, grid_x)
+        [ytg, ysg] = np.meshgrid(transmitter_y, grid_y)
+
+        dist = np.sqrt((xtg - xsg)**2 + (ytg - ysg)**2)
+        integral = (1j * np.pi * self.grid_radius / (2 * self.wave_number)) * \
+            bessel1(1, self.wave_number * self.grid_radius) * hankel1(0, self.wave_number * np.transpose(dist))
+        return integral
---
+++ inverse_problem/models.py
@@ -0,0 +1,80 @@
+""" Class inheritance = Model -> PRytov -> PRytovComplex """
+
+import numpy as np
+
+from config import Config
+
+
+class Model:
+
+    def __init__(self):
+
+        # Physical parameters
+        self.frequency = Config.system["frequency"]
+        self.wavelength = 3e8 / self.frequency
+        self.wave_number = 2 * np.pi / self.wavelength
+
+        # Room parameters
+        self.m = Config.doi["inverse_grids"]
+        self.number_of_grids = self.m ** 2
+
+        # Sensor parameters
+        self.transceiver = Config.sensors["transceivers"]
+        self.number_of_rx = Config.sensors["count"]
+        self.number_of_tx = Config.sensors["count"]
+        self.sensor_positions = Config.sensors["positions"]
+        self.sensor_links = Config.sensors["links"]
+
+        # Other parameters
+        self.nan_remove = True
+
+    def get_model(self, *args, **kwargs):
+        return
+
+    def get_data(self, *args, **kwargs):
+        return
+    #
+    # def remove_nan_values(self, field):
+    #     if self.nan_remove:
+    #         np.fill_diagonal(field, np.nan)
+    #         k = field.reshape(field.size, order='F')
+    #         l = [x for x in k if not np.isnan(x)]
+    #         m = np.reshape(l, (self.number_of_tx, self.number_of_rx-1))
+    #         m = np.transpose(m)
+    #         return m
+    #     if not self.nan_remove:
+    #         field[np.isnan(field)] = 0
+    #         return field
+
+
+class PRytov(Model):
+
+    def get_model(self, direct_field, incident_field, integral_values):
+        A = np.zeros((len(self.sensor_links), self.number_of_grids), dtype=complex)
+        for i, pair in enumerate(self.sensor_links):
+            A[i, :] = np.real(self.wave_number ** 2
+                              * np.divide(np.multiply(integral_values[pair[1], :],
+                                                      np.transpose(incident_field[:, pair[0]])),
+                                          direct_field[pair[1], pair[0]]))
+        return A
+
+    @staticmethod
+    def get_data(total_power, direct_power):
+        data = (total_power - direct_power) / (10 * np.log10(np.exp(2)))
+        data = data.reshape(data.size, order='F')
+        return data
+
+
+class PRytovComplex(PRytov):
+
+    def get_model(self, direct_field, incident_field, integral_values):
+        A = np.zeros((len(self.sensor_links), self.number_of_grids), dtype=complex)
+        for i, pair in enumerate(self.sensor_links):
+            A[i, :] = (self.wave_number ** 2) * np.divide(np.multiply(integral_values[pair[1], :],
+                                                                      np.transpose(incident_field[:, pair[0]])),
+                                                          direct_field[pair[1], pair[0]])
+
+        A_real = np.real(A)
+        A_imag = np.imag(A)
+        A_final = np.concatenate((A_real, -A_imag), axis=1)
+        return A_final
